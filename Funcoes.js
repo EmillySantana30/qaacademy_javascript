4c385d0b8cda9143a0e519ebf97dc296fb790232
EmillySantana30 <emillysantana46@gmail.com>
2023-08-24 19:53:55 -0300

    Funções

--- Funcoes.js
+++ Funcoes.js
@@ -37,3 +37,47 @@ function subtracaoComRetorno(a, b, i) {
 }
 
 console.log(subtracaoComRetorno(30, 15, 5))
+
+//2- Crie um metodo que retorne a multiplicação de 2 valores e imprima o retorno
+function multiplicacao(x, y) {
+    const mult = x * y;
+    return mult;
+}
+
+console.log(multiplicacao(10, 5))
+
+//3-Crie um metodo que retorne uma frase e imprima o retorno
+function imprimirFrase(frase) {
+    return frase;
+}
+console.log(imprimirFrase("O amor é lindo!"));
+
+//4- Faça um metodo sem retorno e sem parametro para ler e exibir uma palavra
+function lerExibirPalavra() {
+    const palavra = ("Ola")
+    console.log("A palavra é: " + palavra)
+}
+lerExibirPalavra();
+
+//5-Faça um metodo com retorno que receba como parametro dois valores e retorne os valores das variaveis, trocados
+function trocarValores(valor1, valor2) {
+    const trocar = valor2;
+    valor2 = valor1;
+    valor1 = trocar
+    console.log('valor1:' + valor1 + ' valor2:' + valor2)
+}
+trocarValores(30, 20);
+
+//6-Faça um metodo com retorno que receba como parametro um numero inteiro e retorne true se o valor do parametro divido por 2 for igual a 0. Caso contrario, retorne false
+function dividirPorDois(numero){
+    return numero;
+}
+const valor= dividirPorDois (15);
+if(valor / 2 == 0){
+    console.log("Igual a zero")
+    return true;
+}
+else{
+    console.log("Retorno Invalido")
+    return false;
+}
