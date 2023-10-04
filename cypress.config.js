701c3b0b20100d283ca28b3678156b332c7bf56c
EmillySantana30 <emillysantana46@gmail.com>
2023-10-04 20:14:17 -0300

    Projeto Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 20000,
   e2e: {
     setupNodeEvents(on, config) {
       const cucumber = require('cypress-cucumber-preprocessor').default
--- cypress/e2e/Pages/ProjetoSpec.js
+++ cypress/e2e/Pages/ProjetoSpec.js
@@ -1,13 +1,11 @@
 export const elementosProjeto = {
     URL: '[https://www.madeiramadeira.com.br/]',
     ofertasDoDia: '[class="cav--c-gqwkJN cav--c-lfsYuX cav--c-lfsYuX-ezNrs-page-home"]',
-    guardaRoupa: '[class="cav--c-gNPphv cav--c-gNPphv-epiGtV-textStyle-bodySmallRegular cav--c-gNPphv-iAsWAM-css"]',
+    mercadoria: '[class="cav--c-gNPphv cav--c-gNPphv-epiGtV-textStyle-bodySmallRegular cav--c-gNPphv-iAsWAM-css"]',
     carrinho: '[class="cav--c-iOefvc cav--c-iOefvc-cYmIRc-variant-ghost cav--c-iOefvc-iPJLV-css button__product--increase"]',
     adicionar: '[class="cav--c-iOefvc cav--c-iOefvc-kAkgtL-variant-secondary cav--c-iOefvc-icLCAaD-css"]',
+    paginaCarrinho: '[class="cav--c-gNPphv cav--c-gNPphv-iiezzIR-css"]',
     remover: '[class="cav--c-iOefvc cav--c-iOefvc-cYmIRc-variant-ghost cav--c-iOefvc-iPJLV-css"]',
     compra: '[class="cav--c-iOefvc cav--c-iOefvc-fGlPcc-variant-primary cav--c-iOefvc-dMKlZS-responsive-true"]',
-    paginaCarrinho: '[class="cav--c-gNPphv cav--c-gNPphv-iiezzIR-css"]',
     email: '[id="E-mail"]',
-
-
 }
